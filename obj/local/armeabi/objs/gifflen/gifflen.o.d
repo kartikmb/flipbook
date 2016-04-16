a7740a8c3455e86c69c412ff841bd5a9eec7f379
kartikmb <kmendira@calpoly.edu>
2016-04-15 18:46:51 -0700

    Added files via upload

---
+++ obj/local/armeabi/objs/gifflen/gifflen.o.d
@@ -0,0 +1,79 @@
+obj/local/armeabi/objs/gifflen/gifflen.o: jni/gifflen.cpp \
+ /cygdrive/c/Users/Kartik/Desktop/android-ndk-r9/platforms/android-14/arch-arm/usr/include/jni.h \
+ jni/neuquant.h \
+ /cygdrive/c/Users/Kartik/Desktop/android-ndk-r9/platforms/android-14/arch-arm/usr/include/stdio.h \
+ /cygdrive/c/Users/Kartik/Desktop/android-ndk-r9/platforms/android-14/arch-arm/usr/include/sys/cdefs.h \
+ /cygdrive/c/Users/Kartik/Desktop/android-ndk-r9/platforms/android-14/arch-arm/usr/include/sys/cdefs_elf.h \
+ /cygdrive/c/Users/Kartik/Desktop/android-ndk-r9/platforms/android-14/arch-arm/usr/include/android/api-level.h \
+ /cygdrive/c/Users/Kartik/Desktop/android-ndk-r9/platforms/android-14/arch-arm/usr/include/sys/types.h \
+ /cygdrive/c/Users/Kartik/Desktop/android-ndk-r9/platforms/android-14/arch-arm/usr/include/stdint.h \
+ /cygdrive/c/Users/Kartik/Desktop/android-ndk-r9/platforms/android-14/arch-arm/usr/include/sys/_types.h \
+ /cygdrive/c/Users/Kartik/Desktop/android-ndk-r9/platforms/android-14/arch-arm/usr/include/machine/_types.h \
+ /cygdrive/c/Users/Kartik/Desktop/android-ndk-r9/platforms/android-14/arch-arm/usr/include/linux/posix_types.h \
+ /cygdrive/c/Users/Kartik/Desktop/android-ndk-r9/platforms/android-14/arch-arm/usr/include/linux/stddef.h \
+ /cygdrive/c/Users/Kartik/Desktop/android-ndk-r9/platforms/android-14/arch-arm/usr/include/linux/compiler.h \
+ /cygdrive/c/Users/Kartik/Desktop/android-ndk-r9/platforms/android-14/arch-arm/usr/include/asm/posix_types.h \
+ /cygdrive/c/Users/Kartik/Desktop/android-ndk-r9/platforms/android-14/arch-arm/usr/include/asm/types.h \
+ /cygdrive/c/Users/Kartik/Desktop/android-ndk-r9/platforms/android-14/arch-arm/usr/include/linux/types.h \
+ /cygdrive/c/Users/Kartik/Desktop/android-ndk-r9/platforms/android-14/arch-arm/usr/include/machine/kernel.h \
+ /cygdrive/c/Users/Kartik/Desktop/android-ndk-r9/platforms/android-14/arch-arm/usr/include/sys/sysmacros.h \
+ /cygdrive/c/Users/Kartik/Desktop/android-ndk-r9/platforms/android-14/arch-arm/usr/include/stdlib.h \
+ /cygdrive/c/Users/Kartik/Desktop/android-ndk-r9/platforms/android-14/arch-arm/usr/include/string.h \
+ /cygdrive/c/Users/Kartik/Desktop/android-ndk-r9/platforms/android-14/arch-arm/usr/include/malloc.h \
+ /cygdrive/c/Users/Kartik/Desktop/android-ndk-r9/platforms/android-14/arch-arm/usr/include/alloca.h \
+ /cygdrive/c/Users/Kartik/Desktop/android-ndk-r9/platforms/android-14/arch-arm/usr/include/strings.h \
+ /cygdrive/c/Users/Kartik/Desktop/android-ndk-r9/platforms/android-14/arch-arm/usr/include/memory.h \
+ jni/dib.h \
+ /cygdrive/c/Users/Kartik/Desktop/android-ndk-r9/platforms/android-14/arch-arm/usr/include/android/log.h
+
+/cygdrive/c/Users/Kartik/Desktop/android-ndk-r9/platforms/android-14/arch-arm/usr/include/jni.h:
+
+jni/neuquant.h:
+
+/cygdrive/c/Users/Kartik/Desktop/android-ndk-r9/platforms/android-14/arch-arm/usr/include/stdio.h:
+
+/cygdrive/c/Users/Kartik/Desktop/android-ndk-r9/platforms/android-14/arch-arm/usr/include/sys/cdefs.h:
+
+/cygdrive/c/Users/Kartik/Desktop/android-ndk-r9/platforms/android-14/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/cygdrive/c/Users/Kartik/Desktop/android-ndk-r9/platforms/android-14/arch-arm/usr/include/android/api-level.h:
+
+/cygdrive/c/Users/Kartik/Desktop/android-ndk-r9/platforms/android-14/arch-arm/usr/include/sys/types.h:
+
+/cygdrive/c/Users/Kartik/Desktop/android-ndk-r9/platforms/android-14/arch-arm/usr/include/stdint.h:
+
+/cygdrive/c/Users/Kartik/Desktop/android-ndk-r9/platforms/android-14/arch-arm/usr/include/sys/_types.h:
+
+/cygdrive/c/Users/Kartik/Desktop/android-ndk-r9/platforms/android-14/arch-arm/usr/include/machine/_types.h:
+
+/cygdrive/c/Users/Kartik/Desktop/android-ndk-r9/platforms/android-14/arch-arm/usr/include/linux/posix_types.h:
+
+/cygdrive/c/Users/Kartik/Desktop/android-ndk-r9/platforms/android-14/arch-arm/usr/include/linux/stddef.h:
+
+/cygdrive/c/Users/Kartik/Desktop/android-ndk-r9/platforms/android-14/arch-arm/usr/include/linux/compiler.h:
+
+/cygdrive/c/Users/Kartik/Desktop/android-ndk-r9/platforms/android-14/arch-arm/usr/include/asm/posix_types.h:
+
+/cygdrive/c/Users/Kartik/Desktop/android-ndk-r9/platforms/android-14/arch-arm/usr/include/asm/types.h:
+
+/cygdrive/c/Users/Kartik/Desktop/android-ndk-r9/platforms/android-14/arch-arm/usr/include/linux/types.h:
+
+/cygdrive/c/Users/Kartik/Desktop/android-ndk-r9/platforms/android-14/arch-arm/usr/include/machine/kernel.h:
+
+/cygdrive/c/Users/Kartik/Desktop/android-ndk-r9/platforms/android-14/arch-arm/usr/include/sys/sysmacros.h:
+
+/cygdrive/c/Users/Kartik/Desktop/android-ndk-r9/platforms/android-14/arch-arm/usr/include/stdlib.h:
+
+/cygdrive/c/Users/Kartik/Desktop/android-ndk-r9/platforms/android-14/arch-arm/usr/include/string.h:
+
+/cygdrive/c/Users/Kartik/Desktop/android-ndk-r9/platforms/android-14/arch-arm/usr/include/malloc.h:
+
+/cygdrive/c/Users/Kartik/Desktop/android-ndk-r9/platforms/android-14/arch-arm/usr/include/alloca.h:
+
+/cygdrive/c/Users/Kartik/Desktop/android-ndk-r9/platforms/android-14/arch-arm/usr/include/strings.h:
+
+/cygdrive/c/Users/Kartik/Desktop/android-ndk-r9/platforms/android-14/arch-arm/usr/include/memory.h:
+
+jni/dib.h:
+
+/cygdrive/c/Users/Kartik/Desktop/android-ndk-r9/platforms/android-14/arch-arm/usr/include/android/log.h:
---
+++ obj/local/armeabi/objs/ndkfoo/ndkfoo.o.d
@@ -0,0 +1,19 @@
+obj/local/armeabi/objs/ndkfoo/ndkfoo.o: jni/ndkfoo.c \
+ /cygdrive/c/Users/Kartik/Desktop/android-ndk-r9/platforms/android-14/arch-arm/usr/include/string.h \
+ /cygdrive/c/Users/Kartik/Desktop/android-ndk-r9/platforms/android-14/arch-arm/usr/include/sys/cdefs.h \
+ /cygdrive/c/Users/Kartik/Desktop/android-ndk-r9/platforms/android-14/arch-arm/usr/include/sys/cdefs_elf.h \
+ /cygdrive/c/Users/Kartik/Desktop/android-ndk-r9/platforms/android-14/arch-arm/usr/include/android/api-level.h \
+ /cygdrive/c/Users/Kartik/Desktop/android-ndk-r9/platforms/android-14/arch-arm/usr/include/malloc.h \
+ /cygdrive/c/Users/Kartik/Desktop/android-ndk-r9/platforms/android-14/arch-arm/usr/include/jni.h
+
+/cygdrive/c/Users/Kartik/Desktop/android-ndk-r9/platforms/android-14/arch-arm/usr/include/string.h:
+
+/cygdrive/c/Users/Kartik/Desktop/android-ndk-r9/platforms/android-14/arch-arm/usr/include/sys/cdefs.h:
+
+/cygdrive/c/Users/Kartik/Desktop/android-ndk-r9/platforms/android-14/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/cygdrive/c/Users/Kartik/Desktop/android-ndk-r9/platforms/android-14/arch-arm/usr/include/android/api-level.h:
+
+/cygdrive/c/Users/Kartik/Desktop/android-ndk-r9/platforms/android-14/arch-arm/usr/include/malloc.h:
+
+/cygdrive/c/Users/Kartik/Desktop/android-ndk-r9/platforms/android-14/arch-arm/usr/include/jni.h:
